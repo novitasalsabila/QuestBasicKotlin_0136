bde8b4cc49cfb29e7602c7e625b83a5e2eedfe0c
novitasalsabila <novnovv10@gmail.com>
2024-10-11 23:43:31 +0700

    Membuat fungsi dengan parameter

--- app/src/main/java/com/example/project1/Fungtions.kt
+++ app/src/main/java/com/example/project1/Fungtions.kt
@@ -5,3 +5,11 @@ fun withoutParameter() {
     println("== withoutParameter ==")
     println("Hello, World!")
 }
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
+
+}
