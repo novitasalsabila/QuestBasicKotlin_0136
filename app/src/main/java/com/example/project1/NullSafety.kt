100b4b6c67158d5e65babaaa80ab5781e242ec63
novitasalsabila <novnovv10@gmail.com>
2024-10-12 00:17:45 +0700

    Fungsi main untuk mejalankan program

--- app/src/main/java/com/example/project1/NullSafety.kt
+++ app/src/main/java/com/example/project1/NullSafety.kt
@@ -13,4 +13,28 @@ fun nullSafety() {
 // This is OK
     nullable = null
 
+// Check for null in conditions
+    if (neverNull == null) {
+        println("inferredNonNull is null")
+    } else {
+        println("inferredNonNull is not null")
+    }
+
+// Safe call operator
+    println(neverNull.length) // 18
+    println(nullable?.length) // null
+
+
+    // notNull doesn't accept null values
+    fun strLength(notNull: String): Int {
+        return notNull.length
+    }
+
+    println(strLength(neverNull)) //18
+
+}
+
+fun main() {
+    nullSafety()
+}
 
