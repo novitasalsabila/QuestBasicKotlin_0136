16087b80b8334cc051635fa97b8301c9e39a30f4
novitasalsabila <novnovv10@gmail.com>
2024-10-12 00:10:37 +0700

    nullable dideklarasikan dengan tipe String?, yang berarti variabel ini dapat berisi nilai string atau null. Mengatur nullable ke null adalah sah.

--- app/src/main/java/com/example/project1/NullSafety.kt
+++ app/src/main/java/com/example/project1/NullSafety.kt
@@ -5,3 +5,12 @@ fun nullSafety() {
 // neverNull has String type
     var neverNull: String = "This can't be null"
 
+// Throws a compiler error
+// neverNull = null
+
+// nullable has nullable String type
+    var nullable: String? = "You can keep a null here"
+// This is OK
+    nullable = null
+
+
