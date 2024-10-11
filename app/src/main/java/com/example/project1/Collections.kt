3426b16d698c6655922ebd1e0f664e2a307d5df2
novitasalsabila <novnovv10@gmail.com>
2024-10-11 22:27:03 +0700

    Membuat Basic Kotlin

---
+++ app/src/main/java/com/example/project1/Collections.kt
@@ -0,0 +1,106 @@
+package com.example.project1
+
+// List adalah Kumpulan data yang disusun secara berurutan dari
+// List bersifat ordered, artinya data yang dimasukkan akan memiliki posisi sesuai urutan
+// List bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// List Read-Only menggunakan listOf
+// List Mutable menggunakan mutableListOf
+
+fun ContohList() {
+    println("=== List ===")
+// List Read-Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+// List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
+// Menambahkan data ke dalam list mutable
+    shape.add("Circle")
+    println(shape)
+
+// Menghapus data dari list Mutable
+    shape.remove("Triangle")
+    println(shape)
+
+// Mengubah data di dalam list Mutable
+    shape[0] = "Oval"
+    println(shape)
+
+// List Read-Only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
+}
+
+// Set adalah Kumpulan data yang tidak memiliki posisi atau urutan tertentu dan hanya menyimpan data unik(tidak ada duplikasi
+// Set bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+// Set bersifat mutable dan read-only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// Set Read-Only menggunakan setOf
+// Set Mutable menggunakan mutableSetOf
+
+fun ContohSet() {
+    println()
+    println("=== Set ===")
+
+    // Set Read-Only
+    val readOnlyAbjad = setOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    // Set Mutable
+    val shape: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    // Menambahkan data ke dalam Set Mutable
+    shape.add("Rectangle")
+    println(shape)
+
+    // Menghapus data dari Set Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    // Set Read-Only
+    val shapesLocked: Set<String> = shape
+    println(shapesLocked)
+}
+
+// Map adalah Kumpulan data yang disusun dalam pasangan key-value
+// Map bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+// Map bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// Map Read-Only menggunakan mapOf
+// Map Mutable menggunakan mutableMapOf
+
+fun ContohMap() {
+
+    println()
+    println("=== Map ===")
+    // Map Read-Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square " to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    // Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    // Menambahkan data ke dalam Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    // Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    // Mengubah data di dalam Map Mutable
+    shape["Square"] = 5
+    println(shape)
+
+    // Map Read-Only
+    val shapesLocked: Map<String, Int> = shape
+    println(shapesLocked)
+}
+
+fun main() {
+    ContohList()
+    ContohSet()
+    ContohMap()
+}
---
+++ app/src/main/java/com/example/project1/TipeData.kt
@@ -0,0 +1,34 @@
+package com.example.project1
+
+fun main() {
+    // Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    // Int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    // Float
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+
+    // Double
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+    // Char
+    val charValue: Char = 'A'
+    println("Char: $charValue")
+
+    // Boolean
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    // String
+    var stringValue: String = "Hello, World!"
+    println("String: $stringValue")
+
+    stringValue = "Hello, Kotlin!"
+    println("String: $stringValue")
+}
